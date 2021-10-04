d2467ee36efd2107225a6bfbf9a2c486ec93383c
Gourishetty Sairam <sairamgourishetty99@gmail.com>
2021-10-04 23:52:49 +0530

    initialised server.js

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+// when we deploy our app to cloud, the server has PORT already defined.
+// So dont override that PORT if its available.
+const PORT = process.env.PORT || 5000
+
+app.get("/", (req, res) => {
+    res.send("My server is set up");
+});
+
+app.listen(PORT, ()=> {
+    console.log(`server is listening on PORT ${PORT}`);
+});
