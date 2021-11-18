51ee553f9c643e3c7a7b9f971ca54e75118d7600
Gourishetty Sairam <sairamgourishetty99@gmail.com>
2021-11-18 22:39:49 +0530

    Register user reducer

--- client/client/src/actions/authActions.js
+++ client/client/src/actions/authActions.js
@@ -1,4 +1,5 @@
-import { SET_CURRENT_USER } from "./types";
+import { SET_CURRENT_USER, SUCCESSFUL_REGISTER, FAILURE_REGISTER, ERRORS } from "./types";
+import axios from "axios";
 
 export const setCurrentUser = (user) => {
     return {
@@ -6,3 +7,24 @@ export const setCurrentUser = (user) => {
         payload: user
     };
 };
+
+export const register = (userData) => dispatch =>  {
+    const config = {
+        headers: {
+            "Content-type": "application/json"
+        }
+    }
+    try {
+        const res = axios.post("localhost:5000/api/users", userData, config)
+        dispatch({
+            type: SUCCESSFUL_REGISTER,
+            payload: res.data
+        });
+    } catch(err) {
+        const error = err.response.data.errors;
+        dispatch({
+            type: FAILURE_REGISTER,
+            payload: error
+        })
+    }
+}
--- client/client/src/reducers/authReducer.js
+++ client/client/src/reducers/authReducer.js
@@ -1,17 +1,34 @@
-import { SET_CURRENT_USER } from "../actions/types";
+import { SET_CURRENT_USER, SUCCESSFUL_REGISTER, ERRORS, FAILURE_REGISTER } from "../actions/types";
 import { isEmpty } from "lodash";
 
 const initialState = {
     isAuthenticated:  false,
+    token: localStorage.getItem("token"),
     user: {}
 }
 
 export default function authReducer(state = initialState, action) {
+    const { payload } = action;
     switch(action.type) {
         case SET_CURRENT_USER:
             return {
                 ...state,
                 isAuthenticated: !isEmpty(action.payload),
+                user: payload
+            }
+        case SUCCESSFUL_REGISTER:
+            localStorage.setItem("token", payload.token);
+            return {
+                ...state,
+                ...payload,
+                isAuthenticated: true,
+            }
+        case ERRORS:
+            localStorage.removeItem("token");
+            return {
+                ...state,
+                token: null,
+                isAuthenticated: false
             }
         default: 
             return state;
